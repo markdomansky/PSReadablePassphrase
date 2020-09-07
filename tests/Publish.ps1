685df073195ffd71ce6c471d3edc759ac9401e3b
Mark Domansky <4317290+markdomansky@users.noreply.github.com>
2020-09-07 12:23:13 -0500

    Slightly revised Test and Publish process.

---
+++ tests/Publish.ps1
@@ -0,0 +1,21 @@
+
+#config.json = {"ApiKey":"PSGalleryAPIKey"}
+$config = convertfrom-json -InputObject (gc .\config.json -raw)
+$outpath = "$psscriptroot\..\Releases"
+$srcpath = "$psscriptroot\..\Module"
+$srcdata = Import-PowerShellDataFile "$srcpath\psreadablepassphrase.psd1"
+$ver = $srcdata.ModuleVersion
+$name = 'PSReadablePassphrase'
+
+if ($env:psmodulepath -notlike "*$outpath*" ) {$env:PSModulePath = $env:PSModulePath + ";$outpath"}
+$targetpath = "$outpath\$name\$ver"
+
+if ((test-path $targetpath)) {remove-item $targetpath -recurse -force}
+
+new-item $targetpath -ItemType Directory
+robocopy /mir $srcpath $targetpath
+
+#publish-module -name $name -nugetapikey $apikey -repository PSGallery
+#-path "C:\Dropbox\Scripts\VB.NET\WebJEA\WebJEAConfig\Module"
+#
+publish-module -path $targetpath -NuGetApiKey $config.apikey -Verbose -Repository PSGallery -whatif
--- tests/RunTests.ps1
+++ tests/RunTests.ps1
@@ -1,21 +1,34 @@
+param([switch]$Publish)
 cd $PSScriptRoot
 #remove module if to ensure we're testing the latest.
 
 $sb = {
+    $ErrorActionPreference = "Stop"
+    $rootpath = $args[0]
+    $publish = $args[1]
     write-host 'moving to tests folder'
-    cd $args[0]
+    cd $rootpath
 
     write-host 'importing pester'
     Import-Module Pester
 
-    # write-host 'importing psreadablepassphrase'
-    # Import-Module ..\module\psreadablepassphrase.psd1 -ErrorAction stop
+    write-host "Increment version"
+    $psdinfo = Import-PowerShellDataFile -Path ..\module\PSReadablePassphrase.psd1
+    [system.version]$ver = $psdinfo.ModuleVersion
+    $newver = [system.version]::new($ver.major,$ver.min,$ver.build+1)
+    Update-ModuleManifest -Path ..\module\PSReadablePassphrase.psd1 -ModuleVersion $newver
+    write-host -ForegroundColor cyan "New Version: $newver"
 
     #run tests
     write-host 'running pester'
     invoke-pester .\psreadablepassphrase.tests.ps1 -verbose
+
+    if ($publish) {
+        write-host "Publish"
+        & .\Publish.ps1
+    }
 }
-$job = start-job -scriptblock $sb -ArgumentList $psscriptroot -name "Tests"
+$job = start-job -scriptblock $sb -ArgumentList $psscriptroot,$publish -name "Tests"
 
 while (($job | get-job).state -ne "Completed") {
     receive-job $job
